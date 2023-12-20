b02cfebae92e8dae6fe0947e33d59695eba129ce
kota <haotaizhongze3@gmail.com>
2023-12-20 16:11:25 +0900

    bunnri

--- readgoodbetaer.ino
+++ readgoodbetaer.ino
@@ -196,29 +196,6 @@ void datelog() {
 }
 
 
-// 吸気バルブの制御
-void openSupplyValve(int targetlegnun) {
-  // 吸気バルブを開く処理
-  digitalWrite(inleg[targetlegnun], HIGH);
-}
-
-void closeSupplyValve(int targetlegnun) {
-  // 吸気バルブを閉じる処理を記述する
-  digitalWrite(inleg[targetlegnun], LOW);
-}
-
-// 排気バルブの制御
-void openExhaustValve(int targetlegnun) {
-  // 排気バルブを開く処理を
-  digitalWrite(EXITleg[targetlegnun], HIGH);
-}
-
-void closeExhaustValve(int targetlegnun) {
-  // 排気バルブを閉じる処理を記述する
-  digitalWrite(EXITleg[targetlegnun], LOW);
-}
-
-
 void controlAirPressure(int i) {
   // 空気圧制御アルゴリズムを実装
   int currentPressure = date[0][i+1];
